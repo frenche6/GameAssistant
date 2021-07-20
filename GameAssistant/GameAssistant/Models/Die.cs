4f5c6ee65bbcd4ed7f3108b2a1ec426055e358a4
Thomas French <frenche6@gmail.com>
2021-07-20 10:24:23 -0400

    Test commit

---
+++ GameAssistant/GameAssistant/Models/Die.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GameAssistant.Models
+{
+    public class Die
+    {
+        public int Sides { get; set; }
+    }
+}
