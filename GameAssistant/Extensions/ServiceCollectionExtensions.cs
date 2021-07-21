225bace4f35cadb54b76ac30624baae04f20f714
Carling Kirk <Carling.Kirk@blackbaud.com>
2021-07-21 17:09:25 -0400

    Add interface for GameStateService and add DI method

---
+++ GameAssistant/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,17 @@
+﻿using GameAssistant.Interfaces;
+using GameAssistant.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace GameAssistant.Extensions
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureGameAssistant<T>(this IServiceCollection services, T gameStateProvider) where T : class, IGameStateProvider
+        {
+            return services
+                .AddTransient<ITurnTracker, TurnTracker>()
+                .AddTransient<IGameStateProvider, T>(provider => gameStateProvider)
+                .AddTransient<IGameStateService, GameStateService>();
+        }
+    }
+}
