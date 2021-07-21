a607bbc912e7c166248c88ee797a3d0e416585a1
TFrench <thomas.french@blackbaud.com>
2021-07-21 18:41:43 -0400

    WIP - Added dicebag

---
+++ GameAssistant/Models/RollHistory.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GameAssistant.Models
+{
+    public class RollHistory<T>
+    {
+        public List<T> DiceRolled { get; set; } = new List<T>();
+        public List<T> ModifiedRolled { get; set; } = new List<T>();
+    }
+}
