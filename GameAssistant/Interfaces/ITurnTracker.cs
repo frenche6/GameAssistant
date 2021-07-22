44864a57410b87fe6344beae16c2f3c0bf9a6ee1
Joe Blanchette <Joseph.Blanchette@blackbaud.me>
2021-07-22 14:59:37 -0400

    Add comments to interfaces

--- GameAssistant/Interfaces/ITurnTracker.cs
+++ GameAssistant/Interfaces/ITurnTracker.cs
@@ -2,10 +2,23 @@
 
 namespace GameAssistant.Interfaces
 {
+    /// <summary>
+    /// Manages turns and applies rules related to turn taking and ending including selecting the next active player on gamestate
+    /// </summary>
     public interface ITurnTracker
     {
+        /// <summary>
+        /// Apply turn rules and manage state 
+        /// </summary>
+        /// <param name="state">The current game state to be modified</param>
+        /// <returns>The modified game state</returns>
         GameState TakeTurn(GameState state);
 
+        /// <summary>
+        /// Sets next player
+        /// </summary>
+        /// <param name="state">The current game state with the current player turn set</param>
+        /// <returns>The new game state</returns>
         GameState EndTurn(GameState state);
     }
 }
